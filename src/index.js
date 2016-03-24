2e53e47e23589a262087507a61e1389113cba475
queicherius <queicherius@gmail.com>
2016-03-24 18:24:06 +0100

    Expose helper

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const cheapestTree = require('./cheapestTree.js')
+const recipeItems = require('./helpers/recipeItems.js')
 
-module.exports = {cheapestTree}
+module.exports = {cheapestTree, recipeItems}
