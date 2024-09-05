0f359dc2b9577c11837c69e2c6d59648861c367b
queicherius <queicherius@gmail.com>
2024-09-05 06:47:57 +0000

    v3.3.0

--- src/cheapestTree.ts
+++ src/cheapestTree.ts
@@ -1,7 +1,7 @@
 import { calculateTreeQuantity } from './calculateTreeQuantity'
 import { calculateTreePrices } from './calculateTreePrices'
 import { calculateTreeCraftFlags } from './calculateTreeCraftFlags'
-import { RecipeTree, RecipeTreeWithCraftFlags } from './types'
+import { RecipeTree, RecipeTreeWithCraftFlags, RecipeTreeWithPrices } from './types'
 import { NestedRecipe } from '@gw2efficiency/recipe-nesting'
 
 export function cheapestTree(
@@ -9,8 +9,22 @@ export function cheapestTree(
   tree: NestedRecipe,
   itemPrices: Record<string, number>,
   availableItems: Record<string, number> = {},
-  forceBuyItems: Array<number> = []
+  forceBuyItems: Array<number> = [],
+  valueOwnItems: string = 'false'
 ): RecipeTreeWithCraftFlags {
+  if (valueOwnItems === 'true') {
+    const treeWithQuantityWithoutAvailableItems = calculateTreeQuantity(
+      amount,
+      tree as RecipeTree,
+      {}
+    )
+    const treeWithPriceWithoutAvailableItems = calculateTreePrices(
+      treeWithQuantityWithoutAvailableItems,
+      itemPrices
+    )
+    const cheaperToBuyItemIds = getCheaperToBuyItemIds(treeWithPriceWithoutAvailableItems)
+    disableCraftForItemIds(tree, cheaperToBuyItemIds)
+  }
   // Adjust the tree total and used quantities
   const treeWithQuantity = calculateTreeQuantity(amount, tree as RecipeTree, availableItems)
 
@@ -31,4 +45,35 @@ export function cheapestTree(
 
   // Recalculate the correct tree price
   return calculateTreePrices(treeWithQuantityPostFlags, itemPrices)
+}
+
+function getCheaperToBuyItemIds(
+  tree: RecipeTreeWithPrices,
+  ids: Array<number> = []
+): Array<number> {
+  if (
+    typeof tree.craftDecisionPrice === 'number' &&
+    typeof tree.buyPrice === 'number' &&
+    tree.craftDecisionPrice * 0.85 > tree.buyPrice
+  ) {
+    if (!ids.includes(tree.id)) {
+      ids.push(tree.id)
+    }
+  }
+
+  if (tree.components && Array.isArray(tree.components)) {
+    tree.components.forEach((component) => getCheaperToBuyItemIds(component, ids))
+  }
+
+  return ids
+}
+
+function disableCraftForItemIds(tree: any, ids: Array<number>) {
+  if (ids.includes(tree.id)) {
+    tree.craft = false
+  }
+
+  if (tree.components && Array.isArray(tree.components)) {
+    tree.components.forEach((component: any) => disableCraftForItemIds(component, ids))
+  }
 }
